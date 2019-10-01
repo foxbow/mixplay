56ab37d0faf244359668cc6db2c3c9ef6c206729
Bjoern Weber <foxbow@web.de>
2019-10-01 17:09:36 +0200

    add initialization for the remote

--- src/mprcinit.c
+++ src/mprcinit.c
@@ -4,8 +4,28 @@
 #include "mphid.h"
 #include "utils.h"
 
+/**
+ * helperfunction for scandir()
+ */
+static int devsel( const struct dirent *entry ) {
+	return( ( entry->d_name[0] != '.' ) &&
+			endsWith( entry->d_name, "kbd" ) );
+}
+
 int main( int argc, char **argv ) {
-	mpconfig_t *config=readConfig( );
+	int numdev=0;
+	int c=-1;
+	int fd=-1;
+	mpconfig_t *config=NULL;
+	struct dirent **devices;
+
+	numdev=scandir( "/dev/input/by-id/", &devices, devsel, alphasort );
+	if( numdev < 1 ) {
+		printf("No input devices available!\n");
+		return -1;
+	}
+
+	config=readConfig( );
 	control=readConfig( );
 	if( control == NULL ) {
 		control=createConfig();
@@ -15,5 +35,30 @@ int main( int argc, char **argv ) {
 		}
 	}
 
+  printf("Select input device:\n");
+	for( i=0; i<numdev; i++ ) {
+		printf("%2i - %s\n", devices[i]->d_name );
+	}
+	printf(" x - cancel\n" );
+	printf("-> "); fflush( stdout );
+	while( c == -1 ) {
+		c=getchar( 1000 );
+		if( c == 'x' ) {
+			c=numdev+1;
+		}
+		else {
+			c=c-'0';
+			if( ( c < 0 ) || ( c > numdev ) {
+				c=-1;
+			}
+		}
+	}
+
+	getConfig()->rcdev=strdup(devices[c]->d_name);
+	fd=initHID();
+	if( fd == -1 ) {
+		printf("Failed to use %s!\n", devices[c]->d_name );
+		return -1;
+	}
 
 }
